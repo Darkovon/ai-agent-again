3e6c970c862af629cdafc0b06ab9259a797c66ee
Darkovon <007search@gmail.com>
2025-11-30 21:30:36 -0500

    added coversations

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import os
+import sys
 
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
@@ -9,8 +11,14 @@ client = genai.Client(api_key=api_key)
 
 
 def main():
+    if not sys.argv[1]:
+        return 1
+    user_prompt = sys.argv[1]
+    messages = [
+        types.Content(role="user", parts=[types.Part(text=user_prompt)])
+    ]
     response = client.models.generate_content(
-        model='gemini-2.0-flash-001', contents='Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.'
+        model='gemini-2.0-flash-001', contents=messages
     )
 
     prompt_tokens = response.usage_metadata.prompt_token_count
