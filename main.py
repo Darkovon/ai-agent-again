8fb7923a230530cf5448d8deb3a1e45445719241
Darkovon <007search@gmail.com>
2025-11-30 21:54:46 -0500

    changed arg handling

--- main.py
+++ main.py
@@ -5,15 +5,24 @@ from dotenv import load_dotenv
 from google import genai
 from google.genai import types
 
-load_dotenv()
-api_key = os.environ.get("GEMINI_API_KEY")
-client = genai.Client(api_key=api_key)
-
 
 def main():
-    if not sys.argv[1]:
-        return 1
-    user_prompt = sys.argv[1]
+    load_dotenv()
+
+    verbose = "--verbose" in sys.argv
+    args = []
+    for arg in sys.argv[1:]:
+        if not arg.startswith("--"):
+            args.append(arg)
+
+    if not args:
+        sys.exit(1)
+
+    api_key = os.environ.get("GEMINI_API_KEY")
+    client = genai.Client(api_key=api_key)
+
+    user_prompt = " ".join(args)
+
     messages = [
         types.Content(role="user", parts=[types.Part(text=user_prompt)])
     ]
@@ -23,9 +32,10 @@ def main():
 
     prompt_tokens = response.usage_metadata.prompt_token_count
     response_tokens = response.usage_metadata.candidates_token_count
-    print(response.text)
-    print(f"Prompt tokens: {prompt_tokens}\nResponse tokens: {response_tokens}")
 
+    print(response.text)
+    if verbose:
+        print(f"User prompt: {sys.argv[1]}\nPrompt tokens: {prompt_tokens}\nResponse tokens: {response_tokens}")
 
 if __name__ == "__main__":
     main()
